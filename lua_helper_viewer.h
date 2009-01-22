40a1c31548f4205f012f54ddd2e7cfeb96ae79c3
Stuart George <sgeorge@mega-tokyo.com>
2009-01-21 19:56:06 -0500

    forgot h file

---
+++ lua_helper_viewer.h
@@ -0,0 +1,12 @@
+#ifndef _LUA_HELPER_VIEWER_H
+#define _LUA_HELPER_VIEWER_H
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+extern void RegisterViewerFuncs(uGlobalData *gb, lua_State *l);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // _LUA_HELPER_VIEWER_H
