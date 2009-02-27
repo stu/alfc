434f6a398ca32e7436f4a88ab5df838c18bd8f11
Stuart George <sgeorge@mega-tokyo.com>
2009-02-27 08:44:20 -0500

    Change of filetypes

--- core_filetypes.lua
+++ core_filetypes.lua
@@ -4,19 +4,20 @@ function DecomposeFiletypes()
 	local filetypes = {}
 
 	filetypes.images = {}
-	filetypes.images.extensions = { "*.gif", "*.png", "*.jpg" }
+	filetypes.images.extensions = { "*.gif", "*.png", "*.jpg", "*.pcx", "*.lbm", "*.psd", "*.raw", "*.nef", "*.jpeg", "*.jpe" }
 	filetypes.images.type = FILETYPE_IMAGE
 
 	filetypes.archives = {}
-	filetypes.archives.extensions = { "*.tar", "*.taz", "*.tar.gz", "*.tar.bz", "*.tar.bz2", "*.tgz", "*.tbz", "*.zip", "*.lha", "*.rar", "*.arc" }
+	filetypes.archives.extensions = { "*.tar", "*.taz", "*.tar.gz", "*.tar.bz", "*.tar.bz2", "*.tgz", "*.tbz",
+										"*.zip", "*.lha", "*.rar", "*.arc", "*.pak", "*.sit", "*.hqx" }
 	filetypes.archives.type = FILETYPE_ARCHIVE
 
 	filetypes.docs = {}
-	filetypes.docs.extensions = { "README", "INSTALL", "*.txt", "*.doc" }
+	filetypes.docs.extensions = { "README", "INSTALL", "*.txt", "*.doc", "*.me" }
 	filetypes.docs.type = FILETYPE_DOC
 
 	filetypes.backup = {}
-	filetypes.backup.extensions = { "*.bak", "*.backup", "*~" }
+	filetypes.backup.extensions = { "*.bak", "*.backup", "*~", "*.old" }
 	filetypes.backup.type = FILETYPE_BACKUP
 
 	local k, v
