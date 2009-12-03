d987d33f9ca5b621039ddf68329629cc7e00ba11
Stuart George <sgeorge@mega-tokyo.com>
2009-12-03 15:42:17 -0500

    Added some extra hints

--- hints.lua
+++ hints.lua
@@ -1,4 +1,7 @@
 hints = {}
-hints[1 + #hints] = "Welcome to ALFM!"
+hints[1 + #hints] = "Welcome to ALFC version " .. VersionString()"
 hints[1 + #hints] = "You can turn hints off in the options.ini file"
+hints[1 + #hints] = "F3 is a quick way top view a file as well as exit the viewer"
+hints[1 + #hints] = "ALT-X is a quick exit"
+hints[1 + #hints] = "F5 for Copy, F6 for Delete, F8 for Move, F12 for tag"
 
