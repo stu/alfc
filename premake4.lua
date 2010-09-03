05e8de6afd2cc04953d3f84eec25efe3cd1517c4
Stuart George <sgeorge@mega-tokyo.com>
2010-09-03 19:30:29 -0400

    Adjusted premake4 build file to build succesfully on windows

--- premake4.lua
+++ premake4.lua
@@ -21,9 +21,10 @@ solution "alfm"
         targetdir "tools"
 
    -- relies on bin2c being built
-	project "alfm"
+	project "alfc"
 		kind "ConsoleApp"
 		language "C"
+		targetdir "."
 		files { "*.h", "*.c", "lua_common_api.c", "lua_common_api.h", "lua_helper.c","lua_helper.h", "lua_helper_viewer.c", "lau_helper_viewer.h", "version.c","version.h"  }
 		excludes { "bin2c.c", "gcomp.c", "guide.c", "guidedump.c", "gdump.c", "ncurses_guide.c", "guide_guide.c" }
 	 	includedirs { "." }
@@ -64,21 +65,20 @@ solution "alfm"
 			end
 		end
 
+		local inclua = os.pathsearch("lualib.h", "/usr/include/lua5.1;/usr/include/lua5;/usr/include/lua;/usr/include;/usr/local/include;/usr/sfw/include;/mingw/include;/mingw/include/lua;/opt/lua/include;/opt/lua5.1/include")
+		includedirs { inclua }
+		libdirs { liblua }
+
 		configuration "linux"
-			local inclua = os.pathsearch("lualib.h", "/usr/include/lua5.1:/usr/include/lua5:/usr/include/lua:/usr/include:/usr/local/include:/usr/sfw/include:/mingw/include:/mingw/include/lua:/opt/lua/include:/opt/lua5.1/include")
-			includedirs { inclua }
-			libdirs { liblua }
-
 			buildoptions { "-D__USE_LARGEFILE64=1","-D_FILE_OFFSET_BITS=64","-DBUILD_UNIXLIKE","-D_FORTIFY_SOURCE=0"}
-
 			if _ARGS[1] == "ncurses" then
-				defines { "DRV_NCURSES"}
+				defines { "DRV_NCURSES","BUILD_UNIXLIKE"}
 				links { "m", "ncurses", libluaxx, "dl", "z" }
 				files { "portability_unix.c", "ncurses_main.c", "ncurses_interface.c" }
 				excludes { "gui_main.c", "guicore.c", "portability_win32.c" }
 				xflag = true
 			elseif _ARGS[1] == "x11" then
-				defines { "DRV_GUI"}
+				defines { "DRV_GUI","BUILD_UNIXLIKE"}
 				links { "m", libluaxx, "X11", "dl", "z" }
 				files { "portability_unix.c", "gui_main.c", "gui_interface.c", "guicore.c" }
 				excludes { "portability_win32.c", "gui_guide.c", "ncurses_main.c", "ncurses_guide.c" }
@@ -86,19 +86,16 @@ solution "alfm"
 			end
 
 		configuration "windows"
-			local inclua = os.pathsearch("lualib.h", "/usr/include/lua5.1;/usr/include/lua5;/usr/include/lua;/usr/include;/usr/local/include;/usr/sfw/include;/mingw/include;/mingw/include/lua;/opt/lua/include;/opt/lua5.1/include")
-			includedirs { inclua }
-			libdirs { liblua }
 			if _ARGS[1] == "pdcurses" then
-				defines { "DRV_NCURSES"}
-				links { "pdcurses", liblua, "regex", "iberty", "kernel32", "z" }
+				defines { "DRV_NCURSES", "BUILD_WIN32"}
+				links { "pdcurses", libluaxx, "regex", "iberty", "kernel32", "z" }
 				files { "portability_win32.c", "ncurses_main.c", "ncurses_interface.c" }
 				excludes { "gui_main.c", "guicore.c", "portability_unix.c" }
 				xflag = true
 			elseif _ARGS[1] == "win32" then
-				defines { "DRV_GUI"}
-				links { liblua,"regex", "iberty", "kernel32","windows","z" }
-				files { "portability_win.c", "gui_main.c", "gui_interface.c", "guicore.c" }
+				defines { "DRV_GUI", "BUILD_WIN32"}
+				links { libluaxx,"regex", "iberty", "kernel32", "z", "user32", "gdi32" }
+				files { "portability_win32.c", "gui_main.c", "gui_interface.c", "guicore.c" }
 				excludes { "portability_unix.c", "gui_guide.c", "ncurses_main.c", "ncurses_guide.c" }
 				xflag = true
 			end
@@ -110,6 +107,7 @@ solution "alfm"
 		files { "guide.c", "guideload.c", "guidedisplay.c", "dlist.c", "version.c", "memwatch.c", "logwrite.c" }
 	  	objdir "obj"
 	  	includedirs { "." }
+	  	targetdir "."
 
 		libluaxx = "lua5"
 		liblua = os.findlib("lua")
@@ -121,21 +119,21 @@ solution "alfm"
 				libluaxx = "lua5.1"
 			end
 		end
-		local inclua = os.pathsearch("lualib.h", "/usr/include/lua5.1:/usr/include/lua5:/usr/include/lua:/usr/include:/usr/local/include:/usr/sfw/include:/mingw/include:/opt")
-		includedirs { inclua }
-		libdirs { liblua }
+
+		local inclua = os.pathsearch("lualib.h", "/usr/include/lua5.1;/usr/include/lua5;/usr/include/lua;/usr/include;/usr/local/include;/usr/sfw/include;/mingw/include;/mingw/include/lua;/opt/lua/include;/opt/lua5.1/include")
+			includedirs { inclua }
+			libdirs { liblua }
 
 		configuration "linux"
 			buildoptions { "-D__USE_LARGEFILE64=1","-D_FILE_OFFSET_BITS=64","-DBUILD_UNIXLIKE","-D_FORTIFY_SOURCE=0"}
-
 			if _ARGS[1] == "ncurses" then
-				defines { "DRV_NCURSES"}
+				defines { "DRV_NCURSES","BUILD_UNIXLIKE"}
 				links { "m", "ncurses", libluaxx, "dl", "z" }
 				files { "portability_unix.c", "ncurses_guide.c", "ncurses_interface.c" }
 				excludes { "gui_main.c", "guicore.c", "portability_win32.c", "gui_guide.c", "gui_interface.c" }
 				xflag = true
 			elseif _ARGS[1] == "x11" then
-				defines { "DRV_GUI"}
+				defines { "DRV_GUI","BUILD_UNIXLIKE"}
 				links { "m", libluaxx, "X11", "dl", "z" }
 				files { "portability_unix.c", "gui_guide.c", "gui_interface.c", "guicore.c" }
 				excludes { "portability_win32.c", "ncurses_guide.c" }
@@ -144,13 +142,13 @@ solution "alfm"
 
 		configuration "windows"
 			if _ARGS[1] == "win32" then
-				defines { "DRV_GUI"}
-				links { liblua,"regex", "iberty", "kernel32","windows","z" }
-				files { "portability_win.c", "gui_main.c", "gui_interface.c", "guicore.c" }
+				defines { "DRV_GUI", "BUILD_WIN32"}
+				links { libluaxx, "regex", "iberty", "kernel32","user32", "gdi32","z" }
+				files { "portability_win32.c", "gui_main.c", "gui_interface.c", "guicore.c" }
 				xflag = true
 			elseif _ARGS[1] == "pdcurses" then
-				defines { "DRV_NCURSES"}
-				links { "pdcurses", liblua, "regex", "iberty", "kernel32", "z" }
+				defines { "DRV_NCURSES", "BUILD_WIN32"}
+				links { "pdcurses", libluaxx, "regex", "iberty", "kernel32", "z" }
 				files { "portability_win32.c", "ncurses_main.c", "ncurses_interface.c" }
 				xflag = true
 			end
@@ -161,7 +159,7 @@ solution "alfm"
 		files { "guidedump.c", "guideload.c", "dlist.c", "memwatch.c", "logwrite.c" }
 	  	objdir "obj"
 	  	links { "z" }
-
+	  	targetdir "."
 
 if _ACTION ~= "clean" then
 	if xflag == false then
