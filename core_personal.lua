ac3b39d369d43cd9b74b14cd57fe0b752c56e667
Stuart George <yakumo9275@gmail.com>
2016-02-16 11:13:34 -0500

    wip

--- core_personal.lua
+++ core_personal.lua
@@ -6,3 +6,21 @@ if DriverName() == "X11" then
 	BindKey(ALFC_KEY_CTRL + string.byte('D'), "Docs", [[SetFilter(""); SetGlob("*.txt"); AddGlob("*.doc"); AddGlob("README");]])
 end
 
+
+function personal_TagAndDelBAK()
+	TagWithGlob("*.bak")
+	--TagWithGlob("*~")
+	
+	-- dont tag directories
+	lstF = GetFileList()
+	for k,v in ipairs(lstF) do
+		if v.tagged == 1 and v.directory == 1 then
+			-- this will flip the tag
+			TagFile(v.name)
+		end
+	end
+	
+	__TagDelete("")
+end
+
+AddCommand(":delbak ","(Personal) Delete bak files", personal_TagAndDelBAK)
